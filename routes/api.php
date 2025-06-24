7bd6310fedb9f917afd13cbc3ae743ea0e93b356
prapti.tuladhar <prapti.tuladhar@worldlink.com.np>
2025-06-24 20:49:37 +0545

    Configured the api.php as the main route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        "message" => "Puupuu I Love You"
+    ]);
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
